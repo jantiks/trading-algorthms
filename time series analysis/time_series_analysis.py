a743ff8afe0fed9905cbb79f695ec8bb95f6bfd8
jantiks <58886410+jantiks@users.noreply.github.com>
2020-09-19 12:43:48 +0400

    added ETS code

--- time series analysis/time_series_analysis.py
+++ time series analysis/time_series_analysis.py
@@ -11,13 +11,23 @@ import matplotlib.pyplot as plt
 # df[["realgdp","trend"]].loc["2000-03-31":].plot()
 # plt.show()
 
-#-------EWMA-------#
-airline = pd.read_csv("airline_passengers.csv",index_col="Month")
-airline.dropna(inplace=True)
-airline.index = pd.to_datetime(airline.index)
-# airline["sma6"] = airline["Thousands of Passengers"].rolling(6).mean()
-# airline["sma12"] = airline["Thousands of Passengers"].rolling(12).mean()
-# airline[["Thousands of Passengers","sma6","sma12"]].plot()
-airline["ewm12"] = airline["Thousands of Passengers"].ewm(span=3).mean()
-airline[["Thousands of Passengers","ewm12"]].plot()
-plt.show()
+# #-------EWMA-------#
+# airline = pd.read_csv("airline_passengers.csv",index_col="Month")
+# airline.dropna(inplace=True)
+# airline.index = pd.to_datetime(airline.index)
+# # airline["sma6"] = airline["Thousands of Passengers"].rolling(6).mean()
+# # airline["sma12"] = airline["Thousands of Passengers"].rolling(12).mean()
+# # airline[["Thousands of Passengers","sma6","sma12"]].plot()
+# airline["ewm12"] = airline["Thousands of Passengers"].ewm(span=3).mean()
+# airline[["Thousands of Passengers","ewm12"]].plot()
+# plt.show()
+
+
+#---------ETS---------#
+# from statsmodels.tsa.seasonal import seasonal_decompose
+# airline = pd.read_csv("airline_passengers.csv",index_col="Month")
+# airline.dropna(inplace=True)
+# airline.index = pd.to_datetime(airline.index)
+# result = seasonal_decompose(airline["Thousands of Passengers"],model="additive")
+# result.plot()
+# plt.show()
