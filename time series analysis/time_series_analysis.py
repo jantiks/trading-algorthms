af5c498d9154ceae2ce44fccb8a6b85c6c417387
jantiks <58886410+jantiks@users.noreply.github.com>
2020-09-21 13:15:41 +0400

    portfolio allocation part one

--- time series analysis/time_series_analysis.py
+++ time series analysis/time_series_analysis.py
@@ -24,10 +24,10 @@ import matplotlib.pyplot as plt
 
 
 #---------ETS---------#
-# from statsmodels.tsa.seasonal import seasonal_decompose
-# airline = pd.read_csv("airline_passengers.csv",index_col="Month")
-# airline.dropna(inplace=True)
-# airline.index = pd.to_datetime(airline.index)
-# result = seasonal_decompose(airline["Thousands of Passengers"],model="additive")
-# result.plot()
-# plt.show()
+from statsmodels.tsa.seasonal import seasonal_decompose
+airline = pd.read_csv("airline_passengers.csv",index_col="Month")
+airline.dropna(inplace=True)
+airline.index = pd.to_datetime(airline.index)
+result = seasonal_decompose(airline["Thousands of Passengers"],model="multiplicative")
+result.plot()
+plt.show()
