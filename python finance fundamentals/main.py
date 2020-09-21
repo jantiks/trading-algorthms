6344a82d2f51b6bc3b84a93c516208c161032b5d
jantiks <58886410+jantiks@users.noreply.github.com>
2020-09-21 13:37:04 +0400

    portfolio allocation part two

--- python finance fundamentals/main.py
+++ python finance fundamentals/main.py
@@ -1,9 +1,10 @@
 import pandas as pd
 import quandl
 import matplotlib.pyplot as plt
+K_DAY = (252**0.5)
 money = 1000000
 start = pd.to_datetime("2012-01-01")
-end = pd.to_datetime("2020-01-01")
+end = pd.to_datetime("2017-01-01")
 aapl = quandl.get("WIKI/AAPL.11",start_date = start,end_date = end)
 csco = quandl.get("WIKI/CSCO.11",start_date = start,end_date = end)
 ibm = quandl.get("WIKI/IBM.11",start_date = start,end_date = end)
@@ -23,8 +24,10 @@ for stock_df in (aapl,csco,ibm,amzn):
     stock_df["Position Values"] = stock_df["Allocation"]*money
 all_pos_vals = (aapl["Position Values"],csco["Position Values"],
                 ibm["Position Values"],amzn["Position Values"])
-postiton_val = pd.concat(all_pos_vals,axis=1)
-postiton_val.columns = ["aapl pos.","csco pos.","ibm pos.","amzn pos."]
-postiton_val["Total sum"] = postiton_val.sum(axis=1)
-postiton_val.drop("Total sum",axis=1).plot()
-plt.show()
+portfolio_val = pd.concat(all_pos_vals,axis=1)
+portfolio_val.columns = ["aapl pos.","csco pos.","ibm pos.","amzn pos."]
+portfolio_val["Total sum"] = portfolio_val.sum(axis=1)
+portfolio_val["Daily return"] = (portfolio_val["Total sum"]/portfolio_val["Total sum"].shift(1))-1
+SR = (portfolio_val["Daily return"].mean())/portfolio_val["Daily return"].std()
+ASR = K_DAY * SR
+print(ASR)
